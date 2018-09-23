1d69d22cc50d6ba69350e24aa844aeb33b8e2285
Ice <aesahkuno@gmail.com>
2018-09-24 00:13:51 +0700

    พิมมใหญ่

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "HELLO";
 class App extends Component{
     render(){
         return(
